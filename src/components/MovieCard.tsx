dddc4cdd4fc92d729fa68cbbe1b7bfcfb7db7f8c
Jordi <misterreco099@gmail.com>
2025-07-15 20:20:36 -0600

    Change for using Movie type

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,8 +1,19 @@
-export default function MovieCard() {
+import Movie from "../Movie"
+
+
+//Warpper type I have to do because of how React interprets types in components
+type MovieProps = {
+	movie: Movie;
+}
+
+export default function MovieCard({movie}: MovieProps) {
 	return (
-		<div className="flex flex-col justify-evenly m-1">
-		  <div className="bg-[#272822] w-80 h-45 rounded-lg"></div>
-		  <p className="text-[#fbca27] font-bold m-1">Movie Title</p>
+		<div className="w-80 flex flex-col justify-evenly m-1">
+		  <div className="bg-[#272822] w-full h-45 rounded-lg"></div>
+		  <div className="w-full">
+		    <p className="w-full text-left truncate overflow-hidden whitespace-nowrap text-[#fbca27] font-bold m-1  pl-1">{movie.title}</p>
+		    <p className="text-left text-gray-500 pl-1" >{movie.studio}</p>
+		  </div>
 		</div>	
 	)
 }
