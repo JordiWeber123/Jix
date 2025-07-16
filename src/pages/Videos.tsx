808e0c176cf582b697404321184efbda349dad62
Jordi <misterreco099@gmail.com>
2025-07-15 20:20:12 -0600

    Variable movies skeleton

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import MovieCard from '../components/MovieCard'
+import MovieCard from '../components/MovieCard.tsx'
+import Movie from '../Movie.tsx'
+import NotFound from "../assets/NotFound.png"
 
 function Videos() {
-	const [count, setCount] = useState(0)
+	const movies :Movie[] = [
+	]
+	if (movies.length === 0) {
+		return (<div className="flex justify-center w-full h-full">
+			<div className='w-1/3 h-1/3 flex-col'>
+				<img src={NotFound} className='object-contain' />
+				<p className='text-white text-2xl'>No videos were found</p>
+				<button className='text-white bg-[#fbca27] text-start text-xl rounded-4xl p-2 m-2'>Open file explorer</button>
+			</div>
+		</div> )
+	}
+
 	return (
 		<div className="flex justify-evenly flex-wrap">
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-			<MovieCard/>
-		
+			{movies.map((movie) => (
+				<MovieCard movie={movie}/>
+			))}
 		</div>
 	)
 }
